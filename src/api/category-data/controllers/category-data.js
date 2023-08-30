265a423eee7c95fdfd8a962f32196400db231891
robert0628 <robertcarnesdev@gmail.com>
2023-08-30 21:47:04 +0500

    all posts category api addition

--- src/api/category-data/controllers/category-data.js
+++ src/api/category-data/controllers/category-data.js
@@ -9,14 +9,34 @@ module.exports = {
     try {
       let { category, page = 1, pageSize = 24 } = ctx.query;
       category = category.split('-').join(' ')
+      if(category === "all posts") {
+        const { rows: posts } = await strapi.db.connection.raw(`
+          SELECT 
+            pp.id, 
+            pp.title, 
+            JSON_BUILD_OBJECT('title', pt.title, 'id', pt.id) as tag,
+            JSON_BUILD_OBJECT('username', pup.username, 'id', pup.id) as by,
+            JSON_BUILD_OBJECT('url', pf.url, 'width', pf.width, 'height', pf.height) as main_image
+          FROM public.posts pp
+          JOIN public.posts_tag_links ptl ON pp.id = ptl.post_id
+          JOIN public.tags pt ON ptl.tag_id = pt.id
+          JOIN public.posts_by_links ppbl ON ppbl.post_id = pp.id
+          JOIN public.up_users pup ON pup.id = ppbl.user_id
+          JOIN public.files_related_morphs pfrm ON pfrm.related_id = pp.id
+          JOIN public.files pf ON pf.id = pfrm.file_id
+          GROUP BY pp.id, pt.title, pup.username, pf.url, pf.width, pf.height, pt.id, pup.id
+          LIMIT ? OFFSET ?
+        `, [pageSize, (page - 1)*pageSize])
+        ctx.body = { posts }
+        return
+      }
       const { rows: posts } = await strapi.db.connection.raw(`
         SELECT 
           pp.id, 
           pp.title, 
           JSON_BUILD_OBJECT('title', pt.title, 'id', pt.id) as tag,
           JSON_BUILD_OBJECT('username', pup.username, 'id', pup.id) as by,
-          JSON_BUILD_OBJECT('url', pf.url, 'width', pf.width, 'height', pf.height) as main_image,
-          COUNT(pp.id) OVER() as total_results
+          JSON_BUILD_OBJECT('url', pf.url, 'width', pf.width, 'height', pf.height) as main_image
         FROM public.posts pp
         JOIN public.posts_tag_links ptl ON pp.id = ptl.post_id
         JOIN public.tags pt ON ptl.tag_id = pt.id
